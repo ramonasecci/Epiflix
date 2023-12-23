2da3a10eddfd4f90b6e77f2011764c0cf84e803b
Ramona Secci <ramonasecci@live.it>
2023-12-23 12:44:20 +0100

    aggiunto search

--- src/components/AllMovies.jsx
+++ src/components/AllMovies.jsx
@@ -1,12 +1,15 @@
 import { Col, Container, Row } from 'react-bootstrap'
 import  MovieList from './MovieList'
+import SearchMovie from './SearchMovie'
 
 
-const AllMovies = () => {
+const AllMovies = (props) => {
+  
   return (
     <Container className="mx-5">
         <Row>       
-          <Col xs={12}>
+          <Col xs={12}>     
+            <SearchMovie />
             <MovieList category="http://www.omdbapi.com/?apikey=1b936975&s=avengers" title="Avengers"/>
             <MovieList category="http://www.omdbapi.com/?apikey=1b936975&s=harry%20potter" title="Harry Potter"/>
             <MovieList category="http://www.omdbapi.com/?apikey=1b936975&s=lord%20of%20the%20rings" title="Lord of the Rings"/>            
--- src/components/SearchMovie.jsx
+++ src/components/SearchMovie.jsx
@@ -1,31 +1,65 @@
-import React from 'react'
-import { Button, Form } from "react-bootstrap"
+import React, { Component } from 'react';
+import SingleMovie from './SingleMovie';
+import { Col, Form, Row } from 'react-bootstrap';
 
-class SearchMovie extends React.Component {
-    render() {
-        return (
-            <div>
-                <Form>
-                    <Form.Control type="text"
-                        placeholder="Cerca..."
-                        onKeyDown={handleKeyPress} />
-                </Form>
-            </div>
-        )
+class SearchMovie extends Component {
+  state = {
+    searchQuery: '',
+    films: [],
+    isLoading: true,
+    isError: false,
+  };
+
+  handleSearch = () => {
+    this.fetchMovies();
+  };
+
+  handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      this.handleSearch();
     }
-}
+  };
 
-function hideMovieList() {
-    document.querySelector('main').classList.add('d-none')
-}
-
-function handleKeyPress(event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        hideMovieList()
-        const searchTerm = event.target.value;
-        console.log(searchTerm)
+  fetchMovies = async () => {
+    try {
+      let response = await fetch(`http://www.omdbapi.com/?apikey=1b936975&s=${this.state.searchQuery}`);
+      if (response.ok) {
+        let films = await response.json();
+        this.setState({ films: films.Search || [], isLoading: false, isError: false });
+      } else {
+        this.setState({ isLoading: false, isError: true });
+      }
+    } catch (error) {
+      this.setState({ isLoading: false, isError: true });
     }
+  };
+
+  render() {
+    let miniFilmsList = this.state.films || [];
+    return (
+      <>
+        <Row className="mb-3 mx-5">
+          <Col>
+            <Form.Group>
+              <Form.Label>Search a Film</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Search here"
+                value={this.state.searchQuery}
+                onChange={(e) => this.setState({ searchQuery: e.target.value })}
+                onKeyDown={this.handleKeyDown}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="flex-nowrap overflow-x-scroll">
+          {miniFilmsList.map((film, index) => (
+            <SingleMovie key={index} film={film} />
+          ))}
+        </Row>
+      </>
+    );
+  }
 }
 
 export default SearchMovie;
